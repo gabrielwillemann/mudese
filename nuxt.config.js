1f1be1343119f430274b1ec0cfff62e526020865
Gabriel Willemann <gabrielw@outlook.com>
2020-07-07 13:59:27 -0300

    Add color theme for mobile devices

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+      { name: 'theme-color', content: '#5b4f9c' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
